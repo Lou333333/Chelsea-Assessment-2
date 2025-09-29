94d7a3c468e4a2e2a5d5fec609e4f840a91db9c3
Lou333333 <louboyd3@gmail.com>
2025-09-29 11:50:11 +1000

    Initial commit

--- SPE5AMS Ass 2.R
+++ SPE5AMS Ass 2.R
@@ -4,4 +4,6 @@ use_git()
 use_github(private=FALSE)
 system("git status")
 browse_github()
+usethis::use_git_remote("origin", url = NULL,
+                         overwrite = TRUE)
 #### Commit to git
